70c8266c830a4f49b7c2071f43e7153f0ed98463
we9402 <kblm157@naver.com>
2018-02-20 10:23:48 +0900

    Test1

--- src/com/hyun/m1/Main.java
+++ src/com/hyun/m1/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		SchoolController school = new SchoolController();
 		school.start();
+		
+		System.out.println("Program Start");
 	}
 }
